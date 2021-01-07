e01109548bd084671fb1b774f27215f4a5a056e2
steven-shoemaker <steven@stevenshoemaker.me>
2021-01-06 23:11:24 -0600

    Update quant_st.py

--- quant_st.py
+++ quant_st.py
@@ -892,7 +892,7 @@ mu = expected_returns.mean_historical_return(df) #returns.mean() * 252
 S = risk_models.sample_cov(df) #Get the sample covariance matrix
 
 ef = EfficientFrontier(mu, S)
-weights = ef.min_sharpe() #Maximize the Sharpe ratio, and get the raw weights
+weights = ef.max_sharpe() #Maximize the Sharpe ratio, and get the raw weights
 cleaned_weights = ef.clean_weights() 
 ef.portfolio_performance(verbose=True)
 
